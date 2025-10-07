c4ffb35a049ec3efd4eef9343a5c6207f2722aed
eguitar <eguitarrocks@yahoo.com>
2025-10-06 20:36:48 -0700

    pizza class complete

--- src/pizza_builder/Pizza.java
+++ src/pizza_builder/Pizza.java
@@ -1,5 +1,19 @@
 package src.pizza_builder;
 
+import java.util.ArrayList;
+
 public class Pizza {
-    
+    private String chainName;
+    private String size;
+    private ArrayList<String> toppings;
+
+    public Pizza(String chainName, String size, ArrayList<String> toppings) {
+        this.chainName = chainName;
+        this.size = size;
+        this.toppings = toppings;
+    }
+
+    public void eat() {
+        System.out.println(chainName + " " + size + " pizza with toppings: " + String.join(", ", toppings));
+    }
 }
